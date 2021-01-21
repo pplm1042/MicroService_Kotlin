68317088341b4821ef1380c2e695a07eebde94df
pplm1042 <pplm1042@naver.com>
2021-01-21 15:41:56 +0900

    fix bug and learning spring boot

--- chapter2/src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
+++ chapter2/src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
@@ -1,20 +1,29 @@
 package com.microservices.chapter2
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
+
+interface ServiceInterface {
+	fun getHello(name : String) : String
+}
 
 @SpringBootApplication
 class Chapter2Application
 
 @Controller
-class FirstController {
-	@RequestMapping(value = "/user", method = arrayOf(RequestMethod.GET))
+class FirstController(val exampleService: ExampleService) {
+	@Autowired
+	lateinit var service: ServiceInterface
+
+	@RequestMapping(value = ["/user/{name}"], method = arrayOf(RequestMethod.GET))
 	@ResponseBody
-	fun hello() = "hello world"
+	fun hello(@PathVariable name: String) = service.getHello(name)
 }
 
 fun main(args: Array<String>) {
