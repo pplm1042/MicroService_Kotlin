28ff74a7f648619d4ab0b6ee818ec84cef122711
pplm1042 <pplm1042@naver.com>
2021-01-30 00:45:32 +0900

    Reactive spring data with MongoDB

---
+++ chapter5/src/main/kotlin/com/microservices/chapter5/CustomerRepository.kt
@@ -0,0 +1,22 @@
+package com.microservices.chapter5
+
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
+import javax.annotation.PostConstruct
+
+@Repository
+class CustomerRepository(private val template: ReactiveMongoTemplate) {
+    companion object {
+        val initialCustomers = listOf(Customer(1, "Kotlin"),
+                Customer(2, "Spring"),
+                Customer(3, "Microservice", Customer.Telephone("+44", "7123456789")))
+    }
+
+    @PostConstruct
+    fun initializeRepository() = initialCustomers.map(Customer::toMono).map(this::create).map(Mono<Customer>::subscribe)
+
+    fun create(customer: Mono<Customer>) = template.save(customer)
+
+}
