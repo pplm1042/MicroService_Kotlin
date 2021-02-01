9eef7b2027b64c9931df674b8cab8677c33a56a7
pplm1042 <pplm1042@naver.com>
2021-02-01 19:04:22 +0900

    CRUD with RESTful API service

--- chapter5/src/main/kotlin/com/microservices/chapter5/CustomerRepository.kt
+++ chapter5/src/main/kotlin/com/microservices/chapter5/CustomerRepository.kt
@@ -1,6 +1,12 @@
 package com.microservices.chapter5
 
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.find
+import org.springframework.data.mongodb.core.findById
+import org.springframework.data.mongodb.core.query.Criteria.where
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.isEqualTo
+import org.springframework.data.mongodb.core.remove
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
@@ -18,5 +24,9 @@ class CustomerRepository(private val template: ReactiveMongoTemplate) {
     fun initializeRepository() = initialCustomers.map(Customer::toMono).map(this::create).map(Mono<Customer>::subscribe)
 
     fun create(customer: Mono<Customer>) = template.save(customer)
-
+    fun findById(id: Int) = template.findById<Customer>(id)
+    fun deleteById(id: Int) =
+            template.remove<Customer>(Query(where("_id").isEqualTo(id)))
+    fun findCustomer(nameFilter: String) = template.find<Customer>(
+            Query(where("name").regex(".*$nameFilter.*", "i")))
 }
